a9186fb636a566ffc3ea2199687df4c283e50cc6
Jyothsna <94163611+JyothsnaVellampalli@users.noreply.github.com>
2022-04-12 09:50:50 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -22,85 +22,85 @@ router.post('/register', async(req,res)=>{
       let pwd = req.body.password; 
       let hashedpwd = await hashedpassword(req.body.password);  
       req.body.password = hashedpwd;
-      req.body.status = "not Verified";
+//       req.body.status = "not Verified";
 
-      //create token , shorturl
-      let url= 'http://localhost:3000/verify?id=';
+//       //create token , shorturl
+//       let url= 'http://localhost:3000/verify?id=';
       
       let token = createtoken(req.body.name,req.body.email,req.body.phone,pwd)
-      .then(data=>req.body.token = data)
-      .then(data=> data=nanoid(10))
-       .then(data=>{console.log('short=',data);
-        req.body.id=data;
-        sendEmail(url+data);})
+      .then(data=>req.body.token = data);
+//       .then(data=> data=nanoid(10))
+//        .then(data=>{console.log('short=',data);
+//         req.body.id=data;
+//         sendEmail(url+data);})
 
     let document = await db.collection('Markdown_viewer').insertOne(req.body);  
        
       res.json({
         statuscode : 200,
         message : 'user registered successfully',
-        status : 'inactive',
+//         status : 'inactive',
         data : document,
        })
       }
   }catch(error){console.log(error);}
 })
 
-//get token parameters
-router.get('/verify', async(req,res)=>{
-  const client= await MongoClient.connect(dburl);
-  const db = await client.db('users');
-  // console.log(req.query.id);
-  if(req.query.id.length==10){
-  let document = await db.collection('Markdown_viewer').findOne({id:req.query.id})
+// //get token parameters
+// router.get('/verify', async(req,res)=>{
+//   const client= await MongoClient.connect(dburl);
+//   const db = await client.db('users');
+//   // console.log(req.query.id);
+//   if(req.query.id.length==10){
+//   let document = await db.collection('Markdown_viewer').findOne({id:req.query.id})
   
-  if(document){
-  res.json({
-    statuscode : 200,
-    token: document.token,
-    name : document.name,
-    email : document.email
-  })
-  }
-  else{
-    res.json({
-      statuscode : 404,
-      message : "url Expired"
-    })
-  }
-  }
+//   if(document){
+//   res.json({
+//     statuscode : 200,
+//     token: document.token,
+//     name : document.name,
+//     email : document.email
+//   })
+//   }
+//   else{
+//     res.json({
+//       statuscode : 404,
+//       message : "url Expired"
+//     })
+//   }
+//   }
   
-})
+// })
 
 
 
-//Verification
-router.get('/verifyuser', async(req,res)=>{
-  if(req.query.token.length > 10){
-   let decode = await verifytoken(req.query.token);
-   console.log(decode);
-  const client= await MongoClient.connect(dburl);
-  const db = await client.db('users');
-   let document = await db.collection('Markdown_viewer').findOne({email: decode.email});
-  // let document = await db.collection('task').findOne({token:req.query.token})
-   console.log({document});
+// //Verification
+// router.get('/verifyuser', async(req,res)=>{
+//   if(req.query.token.length > 10){
+//    let decode = await verifytoken(req.query.token);
+//    console.log(decode);
+//   const client= await MongoClient.connect(dburl);
+//   const db = await client.db('users');
+//    let document = await db.collection('Markdown_viewer').findOne({email: decode.email});
+//   // let document = await db.collection('task').findOne({token:req.query.token})
+//    console.log({document});
 
-  if (document) {
+//   if (document) {
     
-    let update= await db.collection('Markdown_viewer').updateOne({email: document.email},
-      { $set: {name : document.name,email : document.email, phone:document.phone, id: document.id, token: document.token, status:"verified"} })
-    res.json({
-      statuscode: 200,
-      token : document.token,
-    })
-  }
-  else{
-    res.json({
-      statuscode: 400,
-      message : "Invalid key"
-    })
-  }
-}
+//     let update= await db.collection('Markdown_viewer').updateOne({email: document.email},
+//       { $set: {name : document.name,email : document.email, phone:document.phone, id: document.id, token: document.token, status:"verified"} })
+//     res.json({
+//       statuscode: 200,
+//       token : document.token,
+//     })
+//   }
+//   else{
+//     res.json({
+//       statuscode: 400,
+//       message : "Invalid key"
+//     })
+//   }
+// }
 })
 
 
@@ -117,18 +117,18 @@ router.post('/login', async (req, res) => {
     console.log(user);
     let compare = await hashcompare(req.body.password,user.password);
     if(compare){
-      if(user.status=='verified'){
+//       if(user.status=='verified'){
       res.json({
         statuscode: 200,
         message: "login successful"
       })
-    }
-    else{
-      res.json({
-        statuscode: 404,
-        message : "user not verified"
-      })
-    }
+//     }
+//     else{
+//       res.json({
+//         statuscode: 404,
+//         message : "user not verified"
+//       })
+//     }
     }
     else{
       res.json({
@@ -168,18 +168,18 @@ router.post('/forgotpassword',async(req,res)=>{
    
   if(document.phone===req.body.phone && document.name===req.body.name ){
    
-    let url= 'http://localhost:3000/verify?id=';
+//     let url= 'http://localhost:3000/verify?id=';
     let token = await createtoken(req.body.name,req.body.email,req.body.phone,req.body.newpassword)
     .then(data=>req.body.token = data)
-    .then(data=>data=nanoid(10))
-      .then(data=>{
-        req.body.id=data;
-        sendEmail(url+data);})
+//     .then(data=>data=nanoid(10))
+//       .then(data=>{
+//         req.body.id=data;
+//         sendEmail(url+data);})
     
     let hashedpwd = await hashedpassword(req.body.newpassword);
     
     let update= await db.collection('Markdown_viewer').updateOne({email: req.body.email},
-      { $set: {name : req.body.name,email : req.body.email, phone:req.body.phone,password:hashedpwd, id:req.body.id, token:req.body.token, status:"not verified"} });                 
+      { $set: {name : req.body.name,email : req.body.email, phone:req.body.phone,password:hashedpwd, id:req.body.id, token:req.body.token} });                 
     
     res.json({
       statuscode:200,
